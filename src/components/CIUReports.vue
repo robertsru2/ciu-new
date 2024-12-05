871040af577911392993560d689912781f9e4735
Robert Poole <48222588+robertsru2@users.noreply.github.com>
2024-12-05 16:50:04 -0700

    updated ciureports.vue to handle multiselect

--- src/components/CIUReports.vue
+++ src/components/CIUReports.vue
@@ -1,19 +1,32 @@
 <template>
-    <div>
-        <h1>CIU Reports</h1>
-        <p>
-            This is the Clinical Information Update Reports page.
-        </p>
-    </div>
+  <div>
+    <h1>CIU Reports</h1>
+    <p>Clinical Information Update Reports Page.</p>
     <div class="container">
       <div class="report-creation">
-      <input type="date" v-model="startDate" min="2023-05-01" class="date-input">
-      <input type="date" v-model="endDate" min="2023-07-01" class="date-input">
-      <b-button @click="createReports" variant="primary">Create Reports</b-button>
-      <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
-      <div class="form-group">
+        <div class="date-container">
+          <label>Start Date: <input type="date" v-model="startDate" min="2023-05-01" class="date-input"></label>
+          <label>End Date: <input type="date" v-model="endDate" min="2023-07-01" class="date-input"></label>
+        </div>
+        <div class="form-row">
+          <div class="form-group">
+            <label for="departments">Departments:</label>
+            <multiselect 
+              ref="departmentsMultiselect"
+              v-model="selectedDepartments" 
+              :options="departments" 
+              :multiple="true" 
+              :close-on-select="false" 
+              placeholder="Select Department"
+              label="DepartmentLevel"
+              track-by="DepartmentLevel"
+              @update:modelValue="onDepartmentsChange"
+            />
+          </div>
+          <div class="form-group">
             <label for="divisions">Divisions:</label>
             <multiselect 
+              ref="divisionsMultiselect"
               v-model="selectedDivisions" 
               :options="divisions" 
               :multiple="true" 
@@ -21,14 +34,56 @@
               placeholder="Select divisions"
               label="DivisionNM"
               track-by="DivisionNM"
+              @update:modelValue="onDivisionsChange"
             />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="providertype">Provider Type:</label>
+          <multiselect 
+            ref = "providertypesMultiselect"
+            v-model="selectedProviderTypes" 
+            :options="providertypes" 
+            :multiple="true" 
+            :close-on-select="false" 
+            placeholder="Select Provider Type"
+            label="ProviderCategory"
+            track-by="ProviderCategory"
+            @update:modelValue="onProviderTypeChange"
+          />
+        </div>
+        <div class="form-group">
+          <label for="providers">Provider:</label>
+          <multiselect 
+            ref="providersMultiselect"
+            v-model="selectedProviders" 
+            :options="providers" 
+            :multiple="true" 
+            :close-on-select="false" 
+            placeholder="Select Provider(s)"
+            track-by="ProviderID"
+            :custom-label="providerLabel"
+            @update:modelValue="onProvidersChange"
+          >
+            <template #option="{ option }">
+              {{ option.ProviderNM }} - {{ option.ProviderID }}
+            </template>
+            <template #selected="{ option }">
+              {{ option.ProviderNM }} - {{ option.ProviderID }}
+            </template>
+          </multiselect>
+        </div>
+        <div class="progress-container">
+          <p>{{ progress1.step }}</p>
+          <progress :value="progress1.current" :max="progress1.total"></progress>
+          <p>{{ progress1.current }} / {{ progress1.total }}</p>
+        </div>
+        <b-button @click="createReports" variant="primary">Create Reports</b-button>
+        <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
       </div>
-      <div class="progress-container">
-        <p>{{ progress1.step }}</p>
-        <progress :value="progress1.current" :max="progress1.total"></progress>
-        <p>{{ progress1.current }} / {{ progress1.total }}</p>
-      </div>
- 
+    <div class="selection-string">
+      <p>Filter ID Value: {{ filterIDValue }}</p>
+      <p>Filter Level: {{ filterLevel }}</p>
     </div>
     <div class="container">
       <div class="multiselect-container">
@@ -49,11 +104,12 @@
             <p>{{ progress2.current }} / {{ progress2.total }}</p>
         </div>
     </div>
- </div>
-</div>
+    </div>
+  </div>
+  </div>
 </template>
-  
-  <script>
+
+<script>
 import axios from 'axios'
 import Multiselect from 'vue-multiselect'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
@@ -74,7 +130,7 @@ import 'vue-multiselect/dist/vue-multiselect.min.css'
         selectedDepartments: '',
         selectedDivisions: '',
         selectedProviders: '',
-        selectedProviderTypes: 'ALL',    // ALL is the defaul value
+        selectedProviderTypes: '', 
         socket: null,
         socket2: null,
         socket3: null,                
@@ -85,7 +141,9 @@ import 'vue-multiselect/dist/vue-multiselect.min.css'
         endDate: endDate.toISOString().substr(0, 10), // Use the calculated endDate here
         progress1: { current: 0, total: 0, step: 'Report Creation Progress Bar' },
         progress2: { current: 0, total: 0, step: 'Report Printing Progress Bar' },
-        errorMessage: ''
+        errorMessage: '',
+        filterIDValue: 'DOM',
+        filterLevel: 'DepartmentLevel',       // DepartmentLevel, DivisionNM, ProviderID
       }
     },
     async created() {
@@ -102,7 +160,9 @@ import 'vue-multiselect/dist/vue-multiselect.min.css'
         console.log('Provider Types response:', response.data.providertypes); // Debugging line
       } catch (error) {
         console.log('Server is down');
-        this.progress.step = 'API Server Down';
+        this.progress1, 'step', 'API Server Down'; // Ensure progress1 is defined before setting its properties
+        this.progress2, 'step', 'API Server Down'; // Ensure progress2 is defined before setting its properties
+
       }
       try {
         await axios.get('http://localhost:8000/load_files'); // replace with your server's URL
@@ -166,12 +226,47 @@ import 'vue-multiselect/dist/vue-multiselect.min.css'
             this.socket = new WebSocket('ws://localhost:8000/create-reports');
 
             this.socket.onopen = () => {
+            // Build the filterIDValue and filterLevel based on the selected departments, divisions, and providers
+            if (this.selectedDepartments.length > 0) {
+                this.filterIDValue = this.selectedDepartments
+                  .map(department => department.DepartmentLevel)
+                  .join('|') 
+                  + (this.selectedProviderTypes !== 'ALL' 
+                    ? '|' + this.selectedProviderTypes
+                      .map(providertype => providertype.ProviderCategory)
+                      .join('|') 
+                    : '');
+                this.filterLevel = 'DepartmentLevel' + (this.selectedProviderTypes !== 'ALL' ? '|' + 'ProviderCategory' : '');
+                console.log('Selected Departments:', this.selectedDepartments);
+                console.log('Selected Provider Types:', this.selectedProviderTypes);
+              } else if (this.selectedDivisions.length > 0) {
+                this.filterIDValue = this.selectedDivisions
+                  .map(division => division.DivisionNM).join('|') 
+                  + (this.selectedProviderTypes !== 'ALL' 
+                    ? '|' + this.selectedProviderTypes
+                      .map(providertype => providertype.ProviderCategory)
+                      .join('|') 
+                    : '');
+                this.filterLevel = 'DivisionNM' + (this.selectedProviderTypes !== 'ALL' ? '|' + 'ProviderCategory' : '')
+                ;
+            } else if (this.selectedProviders.length > 0) {
+                this.filterIDValue = this.selectedProviders.map(provider =>provider.ProviderID).join('|');
+                this.filterLevel = 'BillingProviderID'
+                console.log('Selected Providers:', this.filterIDValue);
+            } 
+            console.log('Filter ID Value:', this.filterIDValue);
+            console.log('Filter Level:', this.filterLevel);
             const reportRequest = {
                 action: 'createReports',
                 startDate: this.startDate,
                 endDate: this.endDate,
-                filter_level: 'BillingProviderID',
-                filter_id_value: '9999999'    // Pass a number in so RVUGraph error parsing doesn't trigger.
+                filterIDValue: this.filterIDValue,
+                filterLevel: this.filterLevel,       // DepartmentLevel, DivisionNM, BillingProviderID
+
+                //filter_level: 'BillingProviderID',
+                //filter_id_value: '9999999'    // Pass a number in so RVUGraph error parsing doesn't trigger.
+
+
             };
             this.socket.send(JSON.stringify(reportRequest));
             };
@@ -241,88 +336,106 @@ import 'vue-multiselect/dist/vue-multiselect.min.css'
             console.log('WebSocket connection closed');
             };
         },
+        providerLabel(provider) {
+            return `${provider.ProviderNM} - ${provider.ProviderID}`;
+          },
+        onDepartmentsChange() {
+          if (this.selectedDepartments.length > 0) {
+            console.log('Departments changed');      
+            this.selectedDivisions = [];
+            this.selectedProviders = [];
+          } else if (this.selectedDepartments.length === 0) {
+            console.log('Departments changed outside of Length()');
+          }
+        },
+        onDivisionsChange() {
+          if (this.selectedDivisions.length > 0) {
+            console.log('Divisions changed', this.selectedDivisions);
+            this.selectedDepartments = [];
+            this.selectedProviders = [];
+          }
+        },
+        onProviderTypeChange() {
+          if (this.selectedProviderTypes.length > 0) {
+            console.log('Provider Types changed', this.selectedProviderTypes);
+            this.selectedProviders = [];
+          }
+        },
+        onProvidersChange() {
+          if (this.selectedProviders.length > 0) {
+            console.log('Providers changed', this.selectedProviders);
+            this.selectedDepartments = [];
+            this.selectedDivisions = [];
+            this.selectedProviderTypes = [];      
+          }
+        },
     }
   }
   </script>
   
-  <!-- ... -->
-  <style scoped>
+<style>
 .container {
+  margin-top: 20px;
+}
+
+.date-container {
   display: flex;
-  flex-direction: column;
-  align-items: center-start;
-}
-
-.multiselect-container, .progress-container2 {
-  max-height: 50vh;
-  max-width: 50vw;
-  overflow: auto;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-  padding: 0.5rem;
-  margin-bottom: 1rem;
-  background-color: #fff;
-  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-  transition: box-shadow 0.3s ease-in-out;
-  margin-right: 200px;
-}
-.form-check-label {
-  transition: background-color 0.3s ease-in-out;
-}
-
-.form-check-label:hover {
-  background-color: #f8f9fa; /* Change this color to your preference */
-}
-.b-button {
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 0.5rem 1rem;
-  border-radius: 0.25rem;
-  transition: background-color 0.3s ease-in-out;
-}
-
-.report-creation {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 1rem;
-  padding: 1rem;
-  border-bottom: 1px solid #ced4da;
-  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-}
-
-.date-input {
-  margin-right: 1rem;
-}
-.report-container {
-  display: flex;
-  align-items: center;
-}
-
-.progress-container {
-  margin-left: 200px; /* Adjust this value as needed */
-}
-
-.progress-bar-container {
-  margin-left: 20px; /* Adjust this value as needed */
-}
-.button-container {
-  display: flex;
-  justify-content: space-between; /* Or use 'space-around' */
-}
-.form-group {
+  gap: 10px;
   margin-bottom: 15px;
 }
 
+.form-row {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+}
+
+.form-group {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  flex: 1;
+}
+
 .form-group label {
-  display: block;
-  margin-bottom: 5px;
   font-weight: 500;
   color: #333;
 }
 
-.multiselect {
-  width: 200%;
+.wide-multiselect {
+  width: 100%;
+}
+
+.date-input {
+  margin-right: 10px;
+}
+
+.error-message {
+  color: red;
+}
+
+.progress-container {
+  margin-top: 20px;
+}
+
+.multiselect-container {
+  max-height: 200px; /* Set the maximum height */
+  overflow-y: auto; /* Enable vertical scrolling */
+  border: 1px solid #ccc; /* Optional: Add a border for better visibility */
+  padding: 10px; /* Optional: Add padding for better spacing */
+}
+
+.progress-container2 {
+  margin-top: 20px;
+}
+
+.button-container {
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
+}
+
+.progress-bar-container {
+  margin-top: 20px;
 }
 </style>
